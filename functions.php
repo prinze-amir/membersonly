d996552236dd90eca7497d034aaf118df85ab377
Prinze Amir <myprinzeamir@gmail.com>
2023-09-02 20:45:39 -0400

    Initial Commit

---
+++ functions.php
@@ -0,0 +1,46 @@
+<?php
+
+// Connect to the database and return the database object
+$home = '/';
+
+function connect() {
+    // Set the hostname for CodeCademy's platform
+    $hostname = 'localhost';
+
+    // Set the database name
+    $dbname = 'ccuser';
+
+    // Set the username and password with permissions to the database
+    $username = 'ccuser';
+    $password = 'pass';
+    
+    // Create the DSN (data source name) by combining the database type, hostname and dbname
+    $dsn = "pgsql:host=$hostname;dbname=$dbname";
+
+    // Create the try/catch blocks here
+    try{
+       return $db = new PDO($dsn, $username, $password);
+    }
+    catch(Exception $e){
+        echo $e->getMessage();
+    }
+}
+
+// Get a list of all tiers in the database
+function getTiers() {
+    try {
+        // Get the database object
+        $db = connect();
+
+        // Create a query to get all fields for all tiers
+      //  $query = 'SELECT * FROM members';
+        //print_r($db);
+        $tiersQuery = $db->query('SELECT * FROM tiers');
+        // Return all records
+       return $tiersQuery->fetchAll(PDO::FETCH_ASSOC);
+        
+    } catch (Exception $e) {
+        // If an error occurs echo the error
+        echo $e->getMessage();
+    }
+}
